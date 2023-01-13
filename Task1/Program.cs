99a431d1146c18ca6b5ff600825b77bb1cc7c97f
Andrei Krylov <dronovoy@yandex.ru>
2023-01-13 22:21:29 +0300

    Task 1 and 2 added

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Find max");
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine("Max is " + number1);
+}
+else
+{
+    Console.WriteLine("Max is " + number2);
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Find max of 3");
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    if (number1 > number3)
+    {
+        Console.WriteLine("Max is " + number1);
+    }
+    else
+    {
+        Console.WriteLine("Max is " + number3);
+    }
+}
+else
+{
+    if (number2 > number3)
+    {
+    Console.WriteLine("Max is " + number2);
+    }
+    else
+    {
+       Console.WriteLine("Max is " + number3); 
+    }
+}
