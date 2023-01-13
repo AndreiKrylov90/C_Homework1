62a99a89c8c80ca931e25e867a2a82a497af951e
Andrei Krylov <dronovoy@yandex.ru>
2023-01-13 22:29:02 +0300

    Task3 added

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Is your number even?");
+Console.WriteLine("Enter your number");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number % 2) == 0)
+{
+    Console.WriteLine("It is even number");
+}
+else
+{
+    Console.WriteLine("It is not even number");
+}
