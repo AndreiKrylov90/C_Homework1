bad2c8149a6d3978f0e0134278a320336d1269c0
Andrei Krylov <dronovoy@yandex.ru>
2023-01-13 22:43:09 +0300

    Task4 added

---
+++ Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("All even less than yours?");
+Console.WriteLine("Enter your positive number");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i <= number)
+{
+    if (i == number)
+    {
+    Console.Write(i);
+    }
+    else
+    {
+    if ((i + 1) == number)
+    {
+       Console.Write(i); 
+    }
+    else 
+    {
+        Console.Write(i + ", ");
+    }
+    }
+i = i + 2;
+}
